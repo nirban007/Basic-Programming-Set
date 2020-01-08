5db53e437c9827cfe19aa9d59dac4e63f3581ae0
Nirban Mitra <57198481+nirban007@users.noreply.github.com>
2020-01-09 00:07:57 +0600

    Add files via upload

---
+++ input initial name twice.c
@@ -0,0 +1,14 @@
+    #include<stdio.h>
+    #include <string.h>
+    int main(){
+    char name[50];
+
+    gets(name);
+
+    puts(name);
+
+    gets(name);
+
+    puts(name);
+    return 0;
+    }
